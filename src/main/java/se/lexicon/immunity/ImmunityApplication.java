0227a718a2f561cddcad07ecdda39094a004e354
Erik Svensson <eosvensson@gmail.com>
2021-08-23 10:08:33 +0200

    Initial Commit

---
+++ src/main/java/se/lexicon/immunity/ImmunityApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.immunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmunityApplication.class, args);
+    }
+
+}
